0e779ef30f189627b37ac203614f608eddc5da36
sgbasaraner <sarpbasaraner@gmail.com>
2022-03-13 19:45:24 +0300

    add all failing search service tests

--- backend/src/test/java/com/swe573/socialhub/SearchServiceTest.java
+++ backend/src/test/java/com/swe573/socialhub/SearchServiceTest.java
@@ -10,7 +10,6 @@ import com.swe573.socialhub.repository.TagRepository;
 import com.swe573.socialhub.repository.UserRepository;
 import com.swe573.socialhub.service.SearchService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -40,11 +39,9 @@ public class SearchServiceTest {
 
     @Test
     public void SearchService_canFindByServiceHeader() {
-        var sampleEntity = new Service();
-        sampleEntity.setHeader("test");
-        sampleEntity.setId(1L);
+        var sampleEntity = sampleServiceEntity();
 
-        Mockito.when(serviceRepository.findByHeaderLikeIgnoreCase("test"))
+        Mockito.when(serviceRepository.findByHeaderLikeIgnoreCase(sampleEntity.getHeader()))
                 .thenReturn(List.of(sampleEntity));
 
         var expectedResult = List.of(
@@ -53,5 +50,78 @@ public class SearchServiceTest {
         Assertions.assertIterableEquals(expectedResult, result);
     }
 
+    @Test
+    public void SearchService_canFindByServiceDescription() {
+        var sampleEntity = sampleServiceEntity();
 
+        Mockito.when(serviceRepository.findByDescriptionLikeIgnoreCase(sampleEntity.getDescription()))
+                .thenReturn(List.of(sampleEntity));
+
+        var expectedResult = List.of(
+                new SearchMatchDto(sampleEntity.getHeader(), "/service/" + sampleEntity.getId(), SearchMatchType.SERVICE));
+        var result = service.search(sampleEntity.getHeader());
+        Assertions.assertIterableEquals(expectedResult, result);
+    }
+
+    @Test
+    public void SearchService_canFindByUserBio() {
+        var sampleEntity = sampleUserEntity();
+
+        Mockito.when(userRepository.findByBioLikeIgnoreCase(sampleEntity.getBio()))
+                .thenReturn(List.of(sampleEntity));
+
+        var expectedResult = List.of(
+                new SearchMatchDto(sampleEntity.getUsername(), "/user/" + sampleEntity.getId(), SearchMatchType.USER));
+        var result = service.search(sampleEntity.getBio());
+        Assertions.assertIterableEquals(expectedResult, result);
+    }
+
+    @Test
+    public void SearchService_canFindByUserUsername() {
+        var sampleEntity = sampleUserEntity();
+
+        Mockito.when(userRepository.findByUsernameLikeIgnoreCase(sampleEntity.getUsername()))
+                .thenReturn(List.of(sampleEntity));
+
+        var expectedResult = List.of(
+                new SearchMatchDto(sampleEntity.getUsername(), "/user/" + sampleEntity.getId(), SearchMatchType.USER));
+        var result = service.search(sampleEntity.getBio());
+        Assertions.assertIterableEquals(expectedResult, result);
+    }
+
+    @Test
+    public void SearchService_canFindByTagName() {
+        var sampleEntity = sampleTagEntity();
+
+        Mockito.when(tagRepository.findByNameLikeIgnoreCase(sampleEntity.getName()))
+                .thenReturn(List.of(sampleEntity));
+
+        var expectedResult = List.of(
+                new SearchMatchDto(sampleEntity.getName(), "/tags/" + sampleEntity.getId(), SearchMatchType.TAG));
+        var result = service.search(sampleEntity.getName());
+        Assertions.assertIterableEquals(expectedResult, result);
+    }
+
+    private Service sampleServiceEntity() {
+        var sampleEntity = new Service();
+        sampleEntity.setHeader("test");
+        sampleEntity.setId(1L);
+        sampleEntity.setDescription("description");
+        return sampleEntity;
+    }
+
+    private User sampleUserEntity() {
+        var sampleEntity = new User();
+        sampleEntity.setBio("sample bio");
+        sampleEntity.setId(1L);
+        sampleEntity.setUsername("username");
+        return sampleEntity;
+    }
+
+    private Tag sampleTagEntity() {
+        var sampleEntity = new Tag();
+        sampleEntity.setName("Tag");
+        sampleEntity.setId(1L);
+        return sampleEntity;
+    }
 }
