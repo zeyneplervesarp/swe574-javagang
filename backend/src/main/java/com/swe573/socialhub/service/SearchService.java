44a007eb15ce786d8c65bb58bfd2d27a6e2ea67d
sgbasaraner <sarpbasaraner@gmail.com>
2022-03-13 19:14:21 +0300

    add empty method signature

---
+++ backend/src/main/java/com/swe573/socialhub/service/SearchService.java
@@ -0,0 +1,15 @@
+package com.swe573.socialhub.service;
+
+import com.swe573.socialhub.dto.SearchMatchDto;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class SearchService {
+    public List<SearchMatchDto> search(String stringToMatch) {
+        // TODO: implement
+        return Collections.emptyList();
+    }
+}
