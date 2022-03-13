e41a28f26472cd4c022e484238eb94b9cc7c292c
sgbasaraner <sarpbasaraner@gmail.com>
2022-03-13 19:17:05 +0300

    add search service dependencies

--- backend/src/main/java/com/swe573/socialhub/service/SearchService.java
+++ backend/src/main/java/com/swe573/socialhub/service/SearchService.java
@@ -1,6 +1,9 @@
 package com.swe573.socialhub.service;
 
 import com.swe573.socialhub.dto.SearchMatchDto;
+import com.swe573.socialhub.repository.ServiceRepository;
+import com.swe573.socialhub.repository.TagRepository;
+import com.swe573.socialhub.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -8,6 +11,17 @@ import java.util.List;
 
 @Service
 public class SearchService {
+
+    private final UserRepository userRepository;
+    private final TagRepository tagRepository;
+    private final ServiceRepository serviceRepository;
+
+    public SearchService(UserRepository userRepository, TagRepository tagRepository, ServiceRepository serviceRepository) {
+        this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
+        this.serviceRepository = serviceRepository;
+    }
+
     public List<SearchMatchDto> search(String stringToMatch) {
         // TODO: implement
         return Collections.emptyList();
