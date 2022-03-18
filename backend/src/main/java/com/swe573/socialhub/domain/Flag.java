4ca7188caa512e42b52fec08c291e6302a07b6ee
zeynepDYilmaz <yilmaz.zeynepdeniz@gmail.com>
2022-03-18 20:39:39 +0300

    flagDTO not needed

--- backend/src/main/java/com/swe573/socialhub/domain/Flag.java
+++ backend/src/main/java/com/swe573/socialhub/domain/Flag.java
@@ -24,6 +24,14 @@ public class Flag {
         this.flaggedEntity = flaggedEntity;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public FlagType getType() {
         return type;
     }
