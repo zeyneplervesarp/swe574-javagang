731ec01053b3c708cf60eb2f98eb3c921c2d2c80
sgbasaraner <sarpbasaraner@gmail.com>
2022-03-20 10:53:20 +0300

    add constructor

--- backend/src/main/java/com/swe573/socialhub/service/RatingService.java
+++ backend/src/main/java/com/swe573/socialhub/service/RatingService.java
@@ -2,12 +2,23 @@ package com.swe573.socialhub.service;
 
 import com.swe573.socialhub.domain.Rating;
 import com.swe573.socialhub.dto.RatingSummaryDto;
+import com.swe573.socialhub.repository.RatingRepository;
+import com.swe573.socialhub.repository.ServiceRepository;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 
 @Service
 public class RatingService {
+
+    private final RatingRepository repository;
+    private final ServiceRepository serviceRepository;
+
+    public RatingService(RatingRepository repository, ServiceRepository serviceRepository) {
+        this.repository = repository;
+        this.serviceRepository = serviceRepository;
+    }
+
     public Rating addOrUpdateRating(Principal principal, Long serviceId, int rating) {
         return null;
     }
