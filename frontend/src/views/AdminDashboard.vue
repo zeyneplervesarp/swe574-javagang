642b077e44acd35d9191bca0c9ca6b5dab3c4070
ecesari <zeynepecesarioglu@gmail.com>
2022-04-02 18:28:18 +0300

    deleted comments

--- frontend/src/views/AdminDashboard.vue
+++ frontend/src/views/AdminDashboard.vue
@@ -18,78 +18,19 @@
             <div class="row justify-content-center">
               <div class="col-lg-3 order-lg-2"></div>
               <div
-                class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center"
-              >
-                <!-- <div class="card-profile-actions py-4 mt-lg-0">
-                  <base-button
-                    v-if="
-                      !userData.hasServiceRequest &&
-                      !userData.ownsService &&
-                      !serviceData.showServiceButton
-                    "
-                    @click="ConfirmRequest"
-                    type="info"
-                    size="sm"
-                    class="mr-4"
-                    >Send Request to Join</base-button
-                  >
-                  <base-button
-                    disabled
-                    v-if="userData.hasServiceRequest"
-                    type="info"
-                    size="sm"
-                    class="mr-4"
-                    >Already requested to join</base-button
-                  >
-                  <base-button
-                    disabled
-                    v-if="
-                      serviceData.showServiceButton && !userData.ownsService
-                    "
-                    type="warning"
-                    size="sm"
-                    class="mr-4"
-                    >This service has passed</base-button
-                  >
-               
-                </div> -->
+                class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
               </div>
-              <!-- <div class="col-lg-4 order-lg-1">
-                <div class="card-profile-stats d-flex justify-content-center">
-                  <div @click="OpenParticipantModal()">
-                    <a href="#"
-                      ><span class="heading">{{
-                        serviceData.attendingUserCount
-                      }}</span>
-                      <span class="description">Participants</span>
-                    </a>
-                  </div>
-                  <div>
-                    <span class="heading">{{ serviceData.quota }}</span>
-                    <span class="description">Quota</span>
-                  </div>
-                  <div>
-                    <span class="heading">{{ serviceData.minutes }}</span>
-                    <span class="description">Credits</span>
-                  </div>
-                </div>
-              </div> -->
             </div>
             <div class="text-center mt-5">
               <h3>
-                <!-- {{ serviceData.header }}
-                <span class="font-weight-light">
-                  <a :href="'#/profile/' + serviceData.createdUserIdId">
-                    by {{ serviceData.createdUserName }}</a
-                  ></span
-                > -->
+                    Services              
               </h3>
               <div></div>
               <br />
               <div class="text-center">
                 <div>
-                  <table class="table">
-                    <thead class="thead-dark">
+                  <table class="table table-striped">
+                    <thead class="">
                       <tr>
                         <th scope="col">#</th>
                         <th scope="col">Name</th>
@@ -118,7 +59,6 @@
                         <td>
                           <base-button block type="warning" @click="AddFeatured(service.id)" class="mb-3">
                             Featured
-                            {{service.id}}
                           </base-button>
                         </td>
                       </tr>
@@ -128,36 +68,6 @@
               </div>
               <br />
               <div>
-                <!-- <i class="ni ni-square-pin"></i> : {{ serviceData.location }} -->
-                <!-- <i class="ni ni-time-alarm"></i>: {{ serviceData.timeString }} -->
-              </div>
-              <!-- <div>
-                <i class="ni ni-single-02"></i>: {{ serviceData.quota }} people
-              </div> -->
-            </div>
-            <div class="mt-2 py-5 border-top text-center">
-              <div class="row justify-content-center">
-                <div class="col-lg-9">
-                  <p>
-                    <!-- {{ serviceData.description }} -->
-                  </p>
-
-                  <div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="mt-2 py-5 border-top text-center">
-              <div class="row justify-content-center">
-                <div class="col-lg-9">
-                  <p>
-                    Want to get more information about the tags? Click on the
-                    tag that you would like to get more information about.
-                  </p>
-
-                  <div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
