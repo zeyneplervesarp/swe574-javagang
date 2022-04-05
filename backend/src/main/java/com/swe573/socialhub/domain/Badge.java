88e6e559c3d2b3b483acff96b034087eb6f75902
ecesari <zeynepecesarioglu@gmail.com>
2022-04-05 17:52:40 +0300

    badge entity details

--- backend/src/main/java/com/swe573/socialhub/domain/Badge.java
+++ backend/src/main/java/com/swe573/socialhub/domain/Badge.java
@@ -1,5 +1,7 @@
 package com.swe573.socialhub.domain;
 
+import com.swe573.socialhub.enums.BadgeType;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,5 +12,35 @@ public class Badge {
     @ManyToOne
     @JoinColumn(name = "owner")
     User owner;
+    private BadgeType badgeType;
 
+
+    public Badge(User owner, BadgeType badgeType) {
+        this.owner = owner;
+        this.badgeType = badgeType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public BadgeType getBadgeType() {
+        return badgeType;
+    }
+
+    public void setBadgeType(BadgeType badgeType) {
+        this.badgeType = badgeType;
+    }
 }
