67aa5bd6f0d8ca033208417f4d54b310ffc7c376
sgbasaraner <sarpbasaraner@gmail.com>
2022-04-16 07:43:11 +0300

    add feed event enum

---
+++ backend/src/main/java/com/swe573/socialhub/enums/FeedEvent.java
@@ -0,0 +1,16 @@
+package com.swe573.socialhub.enums;
+
+public enum FeedEvent {
+    USER_LOGIN_FAILED,
+    USER_LOGIN_SUCCESSFUL,
+    SERVICE_CREATED,
+    SERVICE_JOIN_REQUESTED,
+    SERVICE_JOIN_APPROVED,
+    EVENT_CREATED,
+    EVENT_JOIN_REQUESTED,
+    EVENT_JOIN_APPROVED,
+
+    // Nice to have, but not in requirements
+    // BADGE_RECEIVED,
+    // USER_REGISTER,
+}
