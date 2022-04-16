e69af57b4e5e503f1e678c8a1007ef7b2c7f7041
sgbasaraner <sarpbasaraner@gmail.com>
2022-04-16 14:41:08 +0300

    add tests for login attempts

--- backend/src/test/java/com/swe573/socialhub/ActivityStreamServiceTests.java
+++ backend/src/test/java/com/swe573/socialhub/ActivityStreamServiceTests.java
@@ -85,6 +85,29 @@ public class ActivityStreamServiceTests {
         Assertions.assertTrue(actorIdList.contains("1"));
     }
 
+    @Test
+    public void ActivityStreamService_canFind_UnsuccessfulLoginAttempts() {
+        var loginUser2 = new User();
+        loginUser2.setId(1L);
+        loginUser2.setUsername("tester2");
+
+        var wrongUserLogin = new LoginAttempt(312L, "tester", LoginAttemptType.WRONG_USERNAME, new Date(System.currentTimeMillis() - 3600 * 1000));
+        var wrongPwLogin = new LoginAttempt(1123L, loginUser2.getUsername(), LoginAttemptType.WRONG_PASSWORD, new Date());
+
+        Mockito.when(loginAttemptRepository.findAllSuccessfulByCreatedBetween(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(List.of(wrongUserLogin, wrongPwLogin));
+
+        Mockito.when(userRepository.findAllByUsername(Mockito.any()))
+                .thenReturn(List.of(loginUser2));
+
+        var response = service.fetchFeed(Set.of(FeedEvent.USER_LOGIN_SUCCESSFUL), new TimestampBasedPagination(null, null, 20, Sort.Direction.ASC));
+        var objectValueTypes = StreamSupport.stream(response.items().spliterator(), false)
+                .map(a -> ((ASObject)((Activity) a).object().iterator().next()).objectTypeString())
+                .collect(Collectors.toList());
+        Assertions.assertEquals(objectValueTypes.size(), 2);
+        Assertions.assertEquals(objectValueTypes.get(0), "login-attempt");
+        Assertions.assertEquals(objectValueTypes.get(1), "user");
+    }
 
 
 }
