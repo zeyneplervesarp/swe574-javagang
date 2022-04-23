b3593c344114379e8755de4006245f6ee1fe5abe
sgbasaraner <sarpbasaraner@gmail.com>
2022-04-23 13:54:40 +0300

    add svc

---
+++ backend/src/main/java/com/swe573/socialhub/service/StatsService.java
@@ -0,0 +1,12 @@
+package com.swe573.socialhub.service;
+
+import com.swe573.socialhub.dto.StatsDto;
+
+import java.security.Principal;
+
+public class StatsService {
+
+    public StatsDto fetchStats(Principal principal) {
+        return null; // TODO: impl
+    }
+}
