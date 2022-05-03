14155f821bb23b485cf1446a2cc59675c9543ce1
sgbasaraner <sarpbasaraner@gmail.com>
2022-05-03 17:49:55 +0300

    implement all scorers for services

--- backend/src/main/java/com/swe573/socialhub/service/SearchPrioritizationService.java
+++ backend/src/main/java/com/swe573/socialhub/service/SearchPrioritizationService.java
@@ -4,9 +4,12 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.swe573.socialhub.domain.*;
+import com.swe573.socialhub.enums.LocationType;
 import com.swe573.socialhub.enums.SearchMatchType;
 import com.swe573.socialhub.repository.UserRepository;
 
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -16,12 +19,14 @@ import java.util.stream.Stream;
 public class SearchPrioritizationService extends CacheLoader<Long, SearchPrioritizationService.SearchPrioritizationParams> {
 
     private final UserRepository userRepository;
+    private final RatingService ratingService;
     private final LoadingCache<Long, SearchPrioritizationParams> prioritizationParamCache = CacheBuilder.newBuilder()
             .expireAfterAccess(2, TimeUnit.MINUTES)
             .build(this);
 
-    public SearchPrioritizationService(UserRepository userRepository) {
+    public SearchPrioritizationService(UserRepository userRepository, RatingService ratingService) {
         this.userRepository = userRepository;
+        this.ratingService = ratingService;
     }
 
     public SearchPrioritizationParams getPrioritizationParams(User user) {
@@ -81,6 +86,7 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
                     PrioritizationScorer currentScorer = null;
                     switch (c) {
                         case NEWCOMER:
+                            currentScorer = new DateProximityScorer(service.getCreatedUser().getCreated());
                             break;
                         case USER_PROFILE_TAGS:
                             currentScorer = new TagPrioritizationScorer(userParams.profileTags, serviceTags);
@@ -95,8 +101,10 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
                             currentScorer = new TagPrioritizationScorer(userParams.followingUsersJoinedServicesTags, serviceTags);
                             break;
                         case RATING:
+                            currentScorer = new RatingScorer(ratingService.getUserRatingSummary(service.getCreatedUser()).getRatingAverage());
                             break;
                         case REPUTATION:
+                            currentScorer = new ReputationScorer(service.getCreatedUser().getReputationPoint());
                             break;
                         case JOINED_SERVICES_TAGS:
                             currentScorer = new TagPrioritizationScorer(userParams.joinedServicesTags, serviceTags);
@@ -105,10 +113,21 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
                             currentScorer = new TagPrioritizationScorer(userParams.givenServicesTags, serviceTags);
                             break;
                         case PROXIMITY:
+                            if (userParams.lastPhysicalService == null || service.getLocationType().equals(LocationType.Online)) {
+                                return 0;
+                            }
+                            currentScorer = new ProximityScorer(
+                                    userParams.lastPhysicalService.getLatitude(),
+                                    userParams.lastPhysicalService.getLongitude(),
+                                    service.getLatitude(),
+                                    service.getLongitude()
+                            );
                             break;
                         case SERVICE_CREATION_DATE:
+                            currentScorer = new DateProximityScorer(service.getCreated());
                             break;
                         case SERVICE_DATE:
+                            currentScorer = new DateProximityScorer(Date.from(service.getTime().atZone(ZoneId.systemDefault()).toInstant()));
                             break;
                     }
                     return currentScorer.getScore() * c.weight;
@@ -163,13 +182,21 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
                         .parallelStream()
         );
 
+        final var userPhysicalSvcsSorted = user
+                .getCreatedServices()
+                .stream()
+                .filter(s -> s.getLocationType().equals(LocationType.Physical))
+                .sorted(Comparator.comparing(Service::getCreated))
+                .collect(Collectors.toUnmodifiableList());
+
         return new SearchPrioritizationParams(
                 followingUserTags,
                 followingUserCreatedServicesTags,
                 joinedServicesTags,
                 createdServicesTags,
                 profileTags,
-                followingUserJoinedServicesTags
+                followingUserJoinedServicesTags,
+                userPhysicalSvcsSorted.isEmpty() ? null : userPhysicalSvcsSorted.get(userPhysicalSvcsSorted.size() - 1)
         );
     }
 
@@ -188,6 +215,7 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
         final Set<String> givenServicesTags;
         final Set<String> profileTags;
         final Set<String> followingUsersJoinedServicesTags;
+        final Service lastPhysicalService;
 
         public SearchPrioritizationParams(
                 Set<String> followingUsersTags,
@@ -195,7 +223,8 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
                 Set<String> joinedServicesTags,
                 Set<String> givenServicesTags,
                 Set<String> profileTags,
-                Set<String> followingUsersJoinedServicesTags
+                Set<String> followingUsersJoinedServicesTags,
+                Service lastPhysicalService
         ) {
             this.followingUsersTags = followingUsersTags;
             this.followingUsersCreatedServicesTags = followingUsersCreatedServicesTags;
@@ -203,6 +232,7 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
             this.givenServicesTags = givenServicesTags;
             this.profileTags = profileTags;
             this.followingUsersJoinedServicesTags = followingUsersJoinedServicesTags;
+            this.lastPhysicalService = lastPhysicalService;
         }
     }
 
@@ -227,6 +257,82 @@ public class SearchPrioritizationService extends CacheLoader<Long, SearchPriorit
             intersection.retainAll(tagsToBeMatched);
 
             return (double) intersection.size() / (double) candidateTags.size();
+        }
+    }
+
+    private static class RatingScorer implements PrioritizationScorer {
+        private final double rating;
+
+        public RatingScorer(double rating) {
+            this.rating = rating;
+        }
+
+        @Override
+        public double getScore() {
+            return rating / 5;
+        }
+    }
+
+    private static class ReputationScorer implements PrioritizationScorer {
+        private final int reputation;
+
+        private static final int REPUTATION_CEILING = 500;
+
+        public ReputationScorer(int reputation) {
+            this.reputation = reputation;
+        }
+
+        @Override
+        public double getScore() {
+            return reputation > REPUTATION_CEILING ? 1 : (double) reputation / REPUTATION_CEILING;
+        }
+    }
+
+    private static class ProximityScorer implements PrioritizationScorer {
+
+        private static final int DISTANCE_CEILING_KM = 100;
+
+        private final double haversineDistanceKilometers;
+
+        public ProximityScorer(double lat1, double lng1, double lat2, double lng2) {
+            // haversine formula
+            // https://en.wikipedia.org/wiki/Haversine_formula
+            final var latDistance = toRad(lat2 - lat1);
+            final var lonDistance = toRad(lng2 - lng1);
+            final var a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
+                    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                            Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+            final var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            final int r = 6371; // radius of the earth
+            haversineDistanceKilometers = r * c;
+        }
+
+        private static double toRad(double value) {
+            return value * Math.PI / 180;
+        }
+
+        @Override
+        public double getScore() {
+            return haversineDistanceKilometers > DISTANCE_CEILING_KM
+                    ? 0
+                    : (DISTANCE_CEILING_KM - haversineDistanceKilometers) / DISTANCE_CEILING_KM;
+        }
+    }
+
+    private static class DateProximityScorer implements PrioritizationScorer {
+
+        private final Date date;
+
+        public DateProximityScorer(Date date) {
+            this.date = date;
+        }
+
+        @Override
+        public double getScore() {
+            final var nowMillis = Instant.now().toEpochMilli();
+            final var creationDateMillis = date.toInstant().toEpochMilli();
+            return creationDateMillis > nowMillis ? 1 : (double) creationDateMillis / (double) nowMillis;
         }
     }
 
